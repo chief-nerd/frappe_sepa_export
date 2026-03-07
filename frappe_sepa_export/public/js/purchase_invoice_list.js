f26d8269ddf50890c436b4c1fa9b3de890ee63f6
Jakob_mimirio <Jakob@mimirio.com>
2026-03-07 16:28:48 +0100

    refactor: Fix syntax error in onload function for SEPA export action in Purchase Invoice List view

--- frappe_sepa_export/public/js/purchase_invoice_list.js
+++ frappe_sepa_export/public/js/purchase_invoice_list.js
@@ -31,7 +31,7 @@ frappe.listview_settings["Purchase Invoice"].onload = function (listview) {
 		}
 		start_bulk_sepa_export(selected);
 	});
-	});
+});
 };
 
 function start_bulk_sepa_export(selected_docs) {
