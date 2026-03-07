8b3803d0c39135690d2501dab1d789a30995dd19
Jakob_mimirio <Jakob@mimirio.com>
2026-03-07 15:42:47 +0100

    refactor: Move module definition creation into after_install function for better setup flow

--- frappe_sepa_export/install/setup.py
+++ frappe_sepa_export/install/setup.py
@@ -3,8 +3,19 @@ import frappe
 
 def after_install():
     """Setup after app installation."""
+    create_module_def()
     frappe.publish_realtime(
         event="msgprint",
         message="SEPA File Export installed. Please configure SEPA Settings for your company.",
         user=frappe.session.user,
     )
+
+
+def create_module_def():
+    """Create the Module Def record if it doesn't exist."""
+    if not frappe.db.exists("Module Def", "SEPA File Export"):
+        module_def = frappe.new_doc("Module Def")
+        module_def.module_name = "SEPA File Export"
+        module_def.app_name = "frappe_sepa_export"
+        module_def.insert(ignore_permissions=True)
+        frappe.db.commit()
